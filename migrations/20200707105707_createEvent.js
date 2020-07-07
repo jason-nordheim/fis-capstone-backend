0e3deed9ec86ddedc03c782efda4c58d6048170b
jason-nordheim <jason.nordheim@gmail.com>
2020-07-07 15:02:22 -0600

    created migration for events table

---
+++ migrations/20200707105707_createEvent.js
@@ -0,0 +1,18 @@
+
+const TABLE_NAME = 'event'
+
+exports.up = function(knex) {
+  return knex.schema.createTable(TABLE_NAME, table => {
+    table.increments('id').primary()
+    table.timestamps() 
+    table.integer('creator').references('id').inTable('user')
+    table.datetime("start", { precision: 6 });
+    table.datetime("end", { precision: 6 });
+    table.string('title')
+    table.string('description')
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists(TABLE_NAME)
+};
