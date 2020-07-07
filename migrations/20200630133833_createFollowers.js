a5945a6abb94d5c7f0682f725d07a1135878218b
jason-nordheim <jason.nordheim@gmail.com>
2020-07-07 15:02:10 -0600

    change timestamp to timestamps

--- migrations/20200630133833_createFollowers.js
+++ migrations/20200630133833_createFollowers.js
@@ -5,7 +5,7 @@ const TABLE_NAME = 'follower'
 exports.up = function(knex) {
   return knex.schema.createTable(TABLE_NAME, table => {
     table.increments('id').primary();
-    table.timestamps("created_at", { precision: 6 })
+    table.timestamps() 
     table.integer("requestee_id").references("id").inTable('user');
     table.integer("requestor_id").references("id").inTable("user");
   })
